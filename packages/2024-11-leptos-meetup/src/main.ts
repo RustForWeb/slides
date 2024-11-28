1e7af71ec7fe94fd56ecfb311c86605737405292
Daniëlle Huisman <danielle@huisman.me>
2024-11-28 09:30:34 +0100

    Add theme and logos

--- packages/2024-11-leptos-meetup/src/main.ts
+++ packages/2024-11-leptos-meetup/src/main.ts
@@ -1,11 +1,14 @@
 import 'reveal.js/dist/reveal.css';
-import 'reveal.js/dist/theme/black.css';
+import './theme/theme.scss';
 
 import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import Notes from 'reveal.js/plugin/notes/notes.esm.js';
 
 const deck = new Reveal({
-    plugins: [Markdown]
+    width: '100%',
+    height: '100%',
+    plugins: [Markdown, Notes]
 });
 
 deck.initialize();
